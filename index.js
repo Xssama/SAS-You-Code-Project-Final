374af617ac6d9ba14719738e90cc8d83fab476f3
Xssama <ouhsaini2005@gmail.com>
2026-09-17 16:31:19 +0100

    Les fonctions rechercherApprenant_ParNom(), ConsulterApprenant() et rechercherApprenant() sont ajoutées et fonctionnent correctement.

--- index.js
+++ index.js
@@ -56,15 +56,54 @@ const apprenants = [
 
 let id = 3;
 
+function normaliserNom(nom) {
+  return nom?.replace(/[^a-zA-Z0-9 ]/g, "").trim();
+}
+
+function calculerProgression(apprenantID) {
+  let apprenantIndex = rechercherApprenant_ParID(apprenantID);
+  if (apprenantIndex != -1) {
+    let Journees_renseignees = apprenants[apprenantIndex].resultats.length;
+    let exercicesTerminesCount = 0;
+    let totalExercicesCount = 0;
+    let challengeTermineCount = 0;
+    for (let index = 0; index < Journees_renseignees; index++) {
+      exercicesTerminesCount +=
+        apprenants[apprenantIndex].resultats[index].exercicesTermines;
+      totalExercicesCount +=
+        apprenants[apprenantIndex].resultats[index].totalExercices;
+      challengeTermineCount += apprenants[apprenantIndex].resultats[index]
+        .challengeTermine
+        ? 1
+        : 0;
+    }
+    let progression = (exercicesTerminesCount / totalExercicesCount) * 100;
+    console.log(
+      apprenants[apprenantIndex].nomComplet,
+      " :",
+      exercicesTerminesCount,
+      "/",
+      totalExercicesCount,
+      "exercices, progression ",
+      progression,
+      " %.\n",
+      Journees_renseignees,
+      "journées renseignées, ",
+      challengeTermineCount,
+      "challenges terminés.",
+    );
+  } else {
+    console.log("L'apprenant n'existe pas!");
+  }
+}
 function ajouterApprenant(nomComplet, ville) {
-  let _nom = nomComplet?.replace(/[^a-zA-Z0-9 ]/g, "").trim();
+  let _nom = normaliserNom(nomComplet);
   let _ville = ville?.replace(/[^a-zA-Z0-9 ]/g, "").trim();
   if (_nom === "" || _ville === "") {
     return -1;
   }
-  const obj = {};
 
-  obj.id = { id: id, nomComplet: _nom, ville: _ville, resultats: [] };
+  const obj = { id: id, nomComplet: _nom, ville: _ville, resultats: [] };
   apprenants.push(obj);
   id++;
   return 1;
@@ -75,7 +114,7 @@ function Afficher_AjouterApprenant() {
   console.log("         Entrer les infos suivante:          ");
   console.log("==============================================");
 
-  const nomComplet = prompt("Nom Complet: ");
+  const nomComplet = normaliserNom(prompt("Nom Complet: "));
   const ville = prompt("Ville: ");
 
   const result = ajouterApprenant(nomComplet, ville);
@@ -112,22 +151,94 @@ function rechercherApprenant_ParID(id) {
   }
   return -1;
 }
-// function rechercherApprenant(cherchePar) {
-//   if (typeof cherchePar == "string") {
-//     for (let index = 0; index < apprenants.length; index++) {
-//       if (apprenants[index].nomComplet.includes(cherchePar)) {
-//         return index;
-//       }
-//     }
-//   } else if (typeof cherchePar == "number") {
-//     for (let index = 0; index < apprenants.length; index++) {
-//       if (apprenants[index].id === cherchePar) {
-//         return index;
-//       }
-//     }
-//     return -1;
-//   }
-// }
+function rechercherApprenant_ParNom(nom) {
+  if (nom === undefined || nom.trim() == "") {
+    console.log("le Nom est invalide ou vide!.");
+    return -1;
+  }
+  let arr = [];
+  for (let index = 0; index < apprenants.length; index++) {
+    let nom_complet = apprenants[index].nomComplet?.toLowerCase();
+    if (nom_complet.includes(nom.toLowerCase())) {
+      arr.push(index);
+    }
+  }
+  return arr.length === 0 ? -1 : arr;
+}
+function ConsulterApprenant(apprenantindex) {
+  if (apprenantindex != -1) {
+    let apprenant = apprenants[apprenantindex];
+    console.log(
+      "------------------------------------------------------------------",
+    );
+    console.log("Nom Complet: ", apprenant.nomComplet);
+    console.log("ID: ", apprenant.id);
+    console.log("Ville: ", apprenant.ville);
+    console.log("Resultats:");
+    if (apprenants[apprenantindex].resultats.length > 0) {
+      console.table(
+        apprenants[apprenantindex].resultats.map((resultats) => ({
+          Jour: resultats.jour,
+          "Exercices Termines": resultats.exercicesTermines,
+          "Exercices Proposees": resultats.totalExercices,
+          Progression:
+            (resultats.exercicesTermines / resultats.totalExercices) * 100 +
+            "%",
+          Challenge: resultats.challengeTermine ? "Oui" : "Non",
+        })),
+      );
+    } else {
+      console.log("(Aucun enregistrement trouvé pour le moment.)");
+    }
+  } else {
+    console.log(
+      "lapprenant avec l'identifiant",
+      apprenantID,
+      " n'existe pas!.",
+    );
+  }
+}
+function rechercherApprenant() {
+  console.log(
+    "------------------------------------------------------------------",
+  );
+  console.log("\t\t\tConsulter un apprenant");
+  console.log(
+    "------------------------------------------------------------------",
+  );
+  let choix = ValiderNombre_Entre(
+    "Tu veux chercher l'apprenant par nom [1] ou par identifiant [2] ? Votre choix : ",
+    1,
+    2,
+  );
+  if (choix === 1) {
+    let nom = normaliserNom(prompt("Le nom d'apprenant?: "));
+
+    let arr = rechercherApprenant_ParNom(nom);
+    if (arr != -1) {
+      console.log(
+        "Les résultats de la recherche : Nombre d'apprenant(s) :",
+        arr.length,
+      );
+      for (let index = 0; index < arr.length; index++) {
+        ConsulterApprenant(arr[index]);
+        console.log(
+          "------------------------------------------------------------------",
+        );
+      }
+    } else {
+      console.log("Aucun résultat trouvé !");
+    }
+  } else {
+    let identifiant = ValiderNombre("L'identifiant d'apprenant?: ");
+    let apprenantIndex = rechercherApprenant_ParID(identifiant);
+    if (apprenantIndex != -1) {
+      ConsulterApprenant(apprenantIndex);
+    } else {
+      console.log("Aucun résultat trouvé !");
+    }
+  }
+}
 function TrouverJour_apprenant(tab, jour) {
   for (let index = 0; index < tab.length; index++) {
     if (tab[index].jour === jour) {
@@ -176,7 +287,6 @@ function enregistrerResultat(
     apprenants[apprenantIndex].resultats.push(nouveauResultats);
     return true;
   }
-  return false;
 }
 
 function AjouterJourResultats(index, jour) {
@@ -210,6 +320,7 @@ function Afficher_enregistrerResultat() {
       )
     ) {
       console.log("Résultat du jour", jour, " enregistré.");
+      calculerProgression(apprenant.id);
     } else {
       console.log(
         "Échec de l'enregistrement des résultats du jour",
@@ -224,5 +335,14 @@ function Afficher_enregistrerResultat() {
 
 //afficherListe_Apprenants();
 //console.log(rechercherApprenant(2));
-console.log(Afficher_enregistrerResultat());
-afficherListe_Apprenants();
+// console.log(Afficher_enregistrerResultat());
+// afficherListe_Apprenants();
+//calculerProgression(2);
+//Afficher_enregistrerResultat();
+//Afficher_AjouterApprenant();
+//afficherListe_Apprenants();
+//console.log(apprenants);
+//console.log(rechercherApprenant_ParNom("oussama"));
+rechercherApprenant();
+//rechercherApprenant();
+//rechercherApprenant_ParNom("sara");
